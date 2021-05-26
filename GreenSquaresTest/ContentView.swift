34b5924d9ce7152b9f054610856f299496059b00
Gregory Keeley <gregoryKeeley@pursuit.org>
2021-05-26 11:27:01 -0400

    - Setup collection view with random colors in SwiftUI

--- GreenSquaresTest/ContentView.swift
+++ GreenSquaresTest/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var colors: [Color] = [ Color(red: 22 / 255, green: 28 / 255, blue: 34 / 255),
+                                    Color(red: 14 / 255, green: 67 / 255, blue: 41 / 255),
+                                    Color(red: 2 / 255, green: 109 / 255, blue: 50 / 255),
+                                    Color(red: 38 / 255, green: 166 / 255, blue: 65 / 255),
+                                    Color(red: 57 / 255, green: 211 / 255, blue: 83 / 255)]
+    private var gridItemLayout = [GridItem(.adaptive(minimum: 20))]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: gridItemLayout, spacing: 0) {
+                ForEach((0...365), id: \.self) {
+                    Image("")
+                        .font(.system(size: 30))
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 20)
+                        .background(colors[($0 + Int(Double.random(in: 1.0 ... 5.0))) % colors.count])
+                        .cornerRadius(4)
+                }
+            }
+        }
     }
 }
 
@@ -19,3 +36,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
