ddf79058ff2c4664aaba0366963692444fedd3d1
Gregory Keeley <gregoryKeeley@pursuit.org>
2021-05-26 11:19:57 -0400

    Initial Commit

---
+++ GreenSquaresTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GreenSquaresTest
+//
+//  Created by Gregory Keeley on 5/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
